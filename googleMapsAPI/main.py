dfe5343371f8dcd5eebdc5acce37055a6007328d
iTzFabregas <fabricio-sampaio@usp.br>
2024-04-11 22:13:09 -0300

    feat: finding the distance from one station to another

--- googleMapsAPI/main.py
+++ googleMapsAPI/main.py
@@ -5,12 +5,32 @@ client = googlemaps.Client(key="AIzaSyBoxKHJNAVlH4Mc8UQAFnC6SrZtaLSgAEg")
 
 # geocode_result = client.geocode("Rua Vergueiro, 1000, São Paulo, SP")
 
-with open("metro_stations.txt", "r") as f:
+stations_dist = []
+
+with open("small_list_station.txt", "r") as f:
     stations_list = f.readlines()
-    for line in stations_list:
-        if (line[0] == "#"):
-            print(line)
-        # TO-DO: calcular a distância
+    n = len(stations_list)
+    for station_1 in range(n):
+        station_2 = station_1 + 1
+
+        if (stations_list[station_1][0] == "#"): continue
+        if (station_2 >= n): break
+        if (stations_list[station_2][0] == "#"): continue
+
+        directions_result = client.directions(stations_list[station_1],
+                                            stations_list[station_2],
+                                            mode="transit",
+                                            transit_mode="subway")
+
+        buffer = {}
+        buffer['origin'] = stations_list[station_1]
+        buffer['destination'] = stations_list[station_2]
+        buffer['distance'] = directions_result[0]["legs"][0]["distance"]["value"]
+        
+        stations_dist.append(buffer)
+
+with open("dist.json", "w") as f:
+    json.dump(stations_dist, f)
 
 '''
 directions_result = client.directions("Estação Tatuape",
