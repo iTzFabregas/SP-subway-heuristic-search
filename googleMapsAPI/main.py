66a8a9b70b3d56dae384333c324f61f6d2bacca8
iTzFabregas <fabricio-sampaio@usp.br>
2024-04-11 22:57:51 -0300

    style: remove comments

--- googleMapsAPI/main.py
+++ googleMapsAPI/main.py
@@ -30,20 +30,3 @@ with open("./stations_list/small_list_station.txt", "r") as f:
 
 with open("dist.json", "w") as f:
     json.dump(stations_dist, f)
-
-'''
-directions_result = client.directions("Estação Tatuape",
-                                    "Estação Corinthians - Itaquera",
-                                    mode="transit",
-                                    transit_mode="subway")
-
-# print(directions_result)
-for etapa in directions_result[0]["legs"][0]["steps"]:
-    print(f"Instrução: {etapa['html_instructions']}")
-    print(f"Distância: {etapa['distance']['text']}")
-    print(f"Duração: {etapa['duration']['text']}")
-    print()
-
-with open("res.json", "w") as f:
-    json.dump(directions_result, f)
-'''
