6117d914f5c3c15f0a54d265783fe2f026e91715
iTzFabregas <fabricio-sampaio@usp.br>
2024-04-13 18:12:39 -0300

    refactor: changing functions

--- googleMapsAPI/main.py
+++ googleMapsAPI/main.py
@@ -1,40 +1,18 @@
 import googlemaps
 import json
-from math import radians, sin, asin, cos, sqrt
+import csv
+
+import heuristic
 
 client = googlemaps.Client(key="")
 
 stations_dist = []
 stations_latlon = []
 
-
-def find_heuristic(directions_result):
-
-    origin_lat = radians(directions_result[0]["legs"][0]["start_location"]["lat"])
-    dest_lat = radians(directions_result[0]["legs"][0]["end_location"]["lat"])
-
-    origin_lng = radians(directions_result[0]["legs"][0]["start_location"]["lng"])
-    dest_lng = radians(directions_result[0]["legs"][0]["end_location"]["lng"])
-
-    return haversine(origin_lat, dest_lat, origin_lng, dest_lng)
-
-def haversine(origin_lat, dest_lat, origin_lng, dest_lng):
-    r = 6_371
-
-    del_lat = origin_lat - dest_lat
-    del_lng = origin_lng - dest_lng
-
-    a = sin(del_lat/2) * sin(del_lat/2) + cos(radians(origin_lat)) * cos(radians(dest_lat)) * sin(del_lng/2) * sin(del_lng/2)
-    c = 2 * asin(sqrt(a))
-
-    return  1000 * r * c
-
-
-
-
 if __name__ == "__main__":
-    with open("./stations_list/small_list_station.txt", "r") as f:
-        stations_list = f.readlines()
+
+    with open("./stations_list/all_stations.txt", "r") as f:
+        stations_list = [line.rstrip('\n') for line in f.readlines()]
         n = len(stations_list)
 
         for station_1 in range(n):
@@ -53,8 +31,8 @@ if __name__ == "__main__":
             buffer['origin'] = stations_list[station_1]
             buffer['destination'] = stations_list[station_2]
             buffer['real-distance'] = directions_result[0]["legs"][0]["distance"]["value"]
-            buffer['heuristic-distance'] = find_heuristic(directions_result)  
-            
+            # buffer['heuristic-distance'] = heuristic.find_heuristic(directions_result)  # testing heurist values
+
             stations_dist.append(buffer)
 
     with open("./output/dist.json", "w") as f:
