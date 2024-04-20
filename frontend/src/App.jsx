c50f2de0da48709998cd71cc5811b7ec4db1de44
iTzFabregas <fabricio-sampaio@usp.br>
2024-04-20 00:42:06 -0300

    feat: starting the website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./assets/tailwind.css"
+
+function App() {
+
+    return (
+        <>
+            <div className="flex flex-col bg-gray-400 w-full h-72 items-center justify-center">
+                <h1 className="text-blue-700 text-9xl">Qual caminho pegar???</h1>
+            </div>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default App
