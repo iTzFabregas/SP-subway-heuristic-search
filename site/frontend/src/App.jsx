ac9549825b2da69a02fc5486c9e07931d65869d7
iTzFabregas <fabricio-sampaio@usp.br>
2024-04-21 23:37:01 -0300

    feat: template of the website

--- site/frontend/src/App.jsx
+++ site/frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
             case "01":
                 // A* no grafo de duração
                 break;
-        
+
             case "02":
                 // A* no grafo de distancia
                 break;
@@ -52,19 +52,19 @@ function App() {
 
     return (
         <div className="flex flex-col items-center">
-            <nav className="flex flex-col bg-gray-400 w-full h-72 items-center justify-center mb-8">
-                <h1 className="text-blue-700 text-9xl">Qual caminho percorrer?</h1>
+            <nav className="flex flex-col bg-01 bg-center w-full h-72 items-center justify-center mb-12 shadow-2xl">
+                <h1 className="text-black font-bold font-mono text-9xl bg-opacity-60 bg-gray-100">Qual caminho percorrer?</h1>
             </nav>
 
-            <div className="flex flex-col bg-red-500 w-1/2 h-96 items-center justify-center mb-8">
+            <div className="flex flex-col bg-gray-200 rounded-3xl shadow-2xl w-1/2 h-96 items-center justify-center mb-12">
                 <h3>Estação de Origem</h3>
-                <input type="text" className="bg-gray-200" onChange={handleOrigin}/>
+                <input type="text" className="bg-gray-400 mb-5" onChange={handleOrigin} />
 
                 <h3>Estação de Destino</h3>
-                <input type="text" className="bg-gray-200" onChange={handleDestination}/>
+                <input type="text" className="bg-gray-400 mb-5" onChange={handleDestination} />
 
                 <h3>Selecione o tipo de viagem</h3>
-                <select defaultValue={"00"}>
+                <select defaultValue={"00"} className="bg-gray-400 p-1 mb-8">
                     <option value="00" onClick={handleOption} disabled>Selecione a viagem</option>
                     <option value="01" onClick={handleOption} >Viagem com menor duração</option>
                     <option value="02" onClick={handleOption} >Viagem com menor caminho percorrido</option>
@@ -73,9 +73,40 @@ function App() {
                 </select>
                 <button className="bg-blue-500 p-1 rounded-2xl" onClick={handleClick}>Encontrar viagem!</button>
             </div>
-
-            <div className="flex flex-col bg-green-500 w-3/4 h-96 items-center justify-center">
-
+            <div className="flex flex-col mb-8 p-10 w-3/4 h-16 bg-neutral-100 rounded-3xl shadow-xl items-center justify-center">
+                <h2>{'Caminho encontrado -> XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'}</h2>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mb-10 px-16 pb-32 w-3/4 bg-neutral-100 rounded-3xl shadow-2xl">
+                <div className="flex flex-col gap-4 border border-black bg-gray-300 mx-10 mt-20 w-3/4 p-3 transform hover:scale-105 transition duration-300">
+                    <h1>Nome da estação 1</h1>
+                    <h2>Endereço da estação</h2>
+                    <h3>Nota</h3>
+                </div>
+                <div className="flex flex-col gap-4 border border-black bg-gray-300 mx-10 mt-20 w-3/4 p-3 transform hover:scale-105 transition duration-300">
+                    <h1>Nome da estação 1</h1>
+                    <h2>Endereço da estação</h2>
+                    <h3>Nota</h3>
+                </div>
+                <div className="flex flex-col gap-4 border border-black bg-gray-300 mx-10 mt-20 w-3/4 p-3 transform hover:scale-105 transition duration-300">
+                    <h1>Nome da estação 1</h1>
+                    <h2>Endereço da estação</h2>
+                    <h3>Nota</h3>
+                </div>
+                <div className="flex flex-col gap-4 border border-black bg-gray-300 mx-10 mt-20 w-3/4 p-3 transform hover:scale-105 transition duration-300">
+                    <h1>Nome da estação 1</h1>
+                    <h2>Endereço da estação</h2>
+                    <h3>Nota</h3>
+                </div>
+                <div className="flex flex-col gap-4 border border-black bg-gray-300 mx-10 mt-20 w-3/4 p-3 transform hover:scale-105 transition duration-300">
+                    <h1>Nome da estação 1</h1>
+                    <h2>Endereço da estação</h2>
+                    <h3>Nota</h3>
+                </div>
+                <div className="flex flex-col gap-4 border border-black bg-gray-300 mx-10 mt-20 w-3/4 p-3 transform hover:scale-105 transition duration-300">
+                    <h1>Nome da estação 1</h1>
+                    <h2>Endereço da estação</h2>
+                    <h3>Nota</h3>
+                </div>
             </div>
         </div>
     )
--- site/frontend/tailwind.config.js
+++ site/frontend/tailwind.config.js
@@ -3,7 +3,14 @@
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        '01': "url('./src/assets/images/01.png')"
+      },
+      from: {
+        '01': "url('./src/assets/images/01.png')"
+      }
+    },
   },
   plugins: [],
 }
